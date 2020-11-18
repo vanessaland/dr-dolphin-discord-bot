23c0e47e73787b321ea7c7cf60ed5efa5eeacbc6
vanessaland <landayan.v@gmail.com>
2020-11-18 00:30:24 -0500

    create discord bot

---
+++ main.js
@@ -0,0 +1,11 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+
+client.once('ready', () => {
+    console.log('Dr. Dolphin is online!');
+});
+
+
+client.login('Nzc4NDg1NDM2Mjg0MTQxNTc4.X7SrDw.1ifs0Lg4H47BUX9zq-6L7YjWYug');
