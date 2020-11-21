8eda1d72e62760ce68363cf6b927679c7337ec12
vanessaland <landayan.v@gmail.com>
2020-11-21 03:13:03 -0500

    permissions

--- commands/google.js
+++ commands/google.js
@@ -2,6 +2,12 @@ module.exports = {
     name: 'google',
     description: 'search for something on google',
     execute(message, args) {
-        message.channel.send('http://google.com/');
+
+        if(message.member.roles.cache.has('779617473901101057')) {
+            message.channel.send('http://google.com/');
+        } else {
+            message.channel.send('You dont have the correct permissions, let me help you :)');
+            message.member.roles.add('779617473901101057');
+        }
     }
 }
